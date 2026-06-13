08476e4d51f2929626142714fe2b017628930f33
thesyntrixi <nivleuiux@gmail.com>
2026-06-14 00:34:12 +0300

    Add eslint-disable comment to fix unused variables

--- app/api/cards/upload/route.ts
+++ app/api/cards/upload/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyToken } from '@/lib/token';
 import { query } from '@/lib/db';
